4d97f13be2428baa8084eac7147bcee192ef3dac
Sebastian Naranjo <naranjos@kidd266-unix1.science.oregonstate.edu>
2019-05-18 14:45:30 -0700

    I wrote a test function for the SBP property. As of now the code does not pass the test.

--- Test_Functions.py
+++ Test_Functions.py
@@ -30,7 +30,7 @@ def test_ConstructDpDm():
     perr = norm(Dp-TDp)
 
     eps = perr+merr
-    assert eps < 0.001
+    assert eps < 0.0001
 
 def test_ConstructPpminv():
     #This routine will test if the inverse of the
@@ -52,25 +52,57 @@ def test_ConstructPpminv():
                                [0,0,0,0,0,2]])/dx
     
     eps          = norm(Ppinv-TPpinv)+norm(Pminv-TPminv)
-    assert eps < 0.001
+    assert eps < 0.0001
 
 
-#def CheckSBPProperty():
-#    #This routine will check that the finite difference 
-#    #operators and the inner product matrices satisfy the
-#    #SBP property at least when N = 4
-#
-#    N  = 4
-#    dx = 2/N
-#
-#    Dp           = ConstructDp(dx,N)
-#    Dm           = ConstructDm(dx,N)
-#    Ppinv, Pminv = ConstructPpminv(dx,N)
-#    Pp           = inv(Ppinv)
-#    Pm           = inv(Pminv)
-#
-#    Qp = Pp.dot(Dp)
-#    Qm = Pm.dot(Dm)
-#
-#    Q = Qp + Qm.transpose()
-#    assert 1 < 2
+def test_SBPProterty():
+    #This routine will check that the finite difference 
+    #operators and the inner product matrices satisfy the
+    #SBP property at least when N = 4
+
+    N  = 4
+    dx = 2/N
+
+    Dp           = ConstructDp(dx,N)
+    Dm           = ConstructDm(dx,N)
+    Ppinv, Pminv = ConstructPpminv(dx,N)
+    Pp           = inv(Ppinv)
+    Pm           = inv(Pminv)
+
+    Qp = Pp.dot(Dp)
+    Qm = Pm.dot(Dm)
+
+    Q = Qp + Qm.transpose()
+    
+    FAlist = [-3,-2,-1,0,1,2,3]
+    LAlist = [-2,-1,0,1,2,3,-3]
+
+    FBlist = [-6,-4,-2,0,2,4,6]
+    LBlist = [6,-6,-4,-2,0,2,4]
+
+    for i in range(6):
+        FA = FAlist[i] 
+        FB = FBlist[i]
+        LA = LAlist[i]
+        LB = LBlist[i]
+        
+        A = np.array([FA,0,0,0,LA])
+        B = np.array([FB,0,0,0,0,LB])
+        
+        err = abs(LB*LA-FB*FA-A.dot(Q.dot(B)))
+        assert err < 0.0001
+
+    for i in range(6):
+        FA = FAlist[i] 
+        FB = FBlist[i]
+        LA = LAlist[i]
+        LB = LBlist[i]
+        
+        A = np.array([FA,6,8,2,LA])
+        B = np.array([FB,-3,-9,2,3,LB])
+        
+        err = abs(LB*LA-FB*FA-A.dot(Q.dot(B)))
+        assert err < 0.0001
+
+
+
